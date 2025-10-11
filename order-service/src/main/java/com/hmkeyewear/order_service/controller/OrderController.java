0f81b78eed6e6524fd7046c885e20f7e3a45129a
Tran Minh Duc <tranminhduc04@gmail.com>
2025-10-11 12:20:07 +0700

    [UPDATE] Fix API-GATEWAY Security

--- order-service/src/main/java/com/hmkeyewear/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/hmkeyewear/order_service/controller/OrderController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.concurrent.ExecutionException;
 
 @RestController
-@RequestMapping("/orders")
+@RequestMapping("/order")
 public class OrderController {
 
     @Autowired
