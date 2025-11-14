95e892255c98c9f4f0c12e799711c9e12a87e4e9
Tran Minh Duc <tranminhduc04@gmail.com>
2025-11-14 20:38:01 +0700

    [ADD] Adding messaging for Brand and Category

--- product-service/src/main/java/com/hmkeyewear/product_service/messaging/CategoryEventConsumer.java
+++ product-service/src/main/java/com/hmkeyewear/product_service/messaging/CategoryEventConsumer.java
@@ -1,4 +1,15 @@
 package com.hmkeyewear.product_service.messaging;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+
 public class CategoryEventConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CategoryEventConsumer.class);
+
+    @RabbitListener(queues = {"${app.rabbitmq.category.queue}"})
+    public void consume(Object message) {
+        LOGGER.info("Received message -> {}",message.toString());
+    }
 }
