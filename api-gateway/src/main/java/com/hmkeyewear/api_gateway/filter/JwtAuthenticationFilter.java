91702d68e83f7ff6eeac81903158e2dbc5d89bbc
Tran Minh Duc <tranminhduc04@gmail.com>
2026-01-07 14:36:30 +0700

    feat(payment): add VNPay IPN with signature verification and idempotency using Firestore

--- api-gateway/src/main/java/com/hmkeyewear/api_gateway/filter/JwtAuthenticationFilter.java
+++ api-gateway/src/main/java/com/hmkeyewear/api_gateway/filter/JwtAuthenticationFilter.java
@@ -28,6 +28,8 @@ public class JwtAuthenticationFilter implements GlobalFilter, Ordered {
             "/auth/forgot-password",
             "/auth/verify-otp",
             "/auth/reset-password",
+            "/payment/vn-pay-ipn",
+            "/payment/vn-pay-callback",
             "/banner/active",
             "/blog/active",
             "/blog/get",
