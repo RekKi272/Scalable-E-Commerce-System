1c7618109d409633b20aa7caf6eb0bde8980420c
Tran Minh Duc <tranminhduc04@gmail.com>
2026-01-29 02:16:12 +0700

    fix(bug): fix cart wrong variables

--- order-service/src/main/java/com/hmkeyewear/order_service/service/OrderService.java
+++ order-service/src/main/java/com/hmkeyewear/order_service/service/OrderService.java
@@ -67,7 +67,7 @@ public class OrderService {
         // BASIC INFO
         order.setUserId(dto.getUserId());
         order.setEmail(dto.getEmail());
-        order.setFullname(dto.getFullname());
+        order.setFullName(dto.getFullName());
         order.setPhone(dto.getPhone());
         order.setPaymentMethod(dto.getPaymentMethod());
         order.setDiscount(dto.getDiscount()); // object
@@ -121,7 +121,7 @@ public class OrderService {
 
         order.setUserId(dto.getUserId());
         order.setEmail(dto.getEmail());
-        order.setFullname(dto.getFullname());
+        order.setFullName(dto.getFullName());
         order.setPhone(dto.getPhone());
         order.setPaymentMethod(dto.getPaymentMethod());
         order.setNote(dto.getNote());
@@ -252,7 +252,7 @@ public class OrderService {
 
         // -------- UPDATE BASIC INFO --------
         order.setEmail(dto.getEmail());
-        order.setFullname(dto.getFullname());
+        order.setFullName(dto.getFullName());
         order.setPhone(dto.getPhone());
         order.setNote(dto.getNote());
 
